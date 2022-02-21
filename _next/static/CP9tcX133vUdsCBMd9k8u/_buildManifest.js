e49802149b22acfbd730ec667eb5e1902b996694
me-manikanta <manikantajreinstein97@gmail.com>
2022-02-21 15:44:42 +0000

    Deploying to gh-pages from @ me-manikanta/me-manikanta.github.io@34826f7e280a4a0592c3a6731e75219704cc7691 🚀

--- _next/static/CP9tcX133vUdsCBMd9k8u/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/1363c2c8377ee67e.css","static/chunks/pages/index-407c2a8d118a8eb1.js"],"/_error":["static/chunks/pages/_error-2f883067a14f4c4a.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
