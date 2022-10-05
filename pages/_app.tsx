b07c82291a7793e478008b3e95438d1132990446
Manikanta Inugurthi <manikantajreinstein97@gmail.com>
2022-10-05 16:38:45 +0530

    :sparkles: Add NavBar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,19 +1,9 @@
 import type { AppProps } from "next/app";
 import "../styles/globals.css";
-import styles from "../styles/Home.module.css";
-import { Footer } from "../components/Footer";
+import { Header } from "../components/Header";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <div className={styles.wrapperNormal}>
-      <div className={`${styles.page} ${styles.home}`}>
-        <header className={`${styles.headerHome} ${styles.animated}`}>
-          <Component {...pageProps} />
-        </header>
-      </div>
-      <Footer />
-    </div>
-  );
+  return <Header />;
 }
 
 export default MyApp;
