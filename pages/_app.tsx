38005ba1843668f4218ae7f3845a2f2def10ee8b
Manikanta Inugurthi <manikantajreinstein97@gmail.com>
2022-10-05 17:32:05 +0530

    :sparkles: Add Toggle button to switch theme

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,14 @@
 import type { AppProps } from "next/app";
 import "../styles/globals.css";
 import { Header } from "../components/Header";
+import { ThemeProvider } from "next-themes";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Header />;
+  return (
+    <ThemeProvider attribute="class">
+      <Header />
+    </ThemeProvider>
+  );
 }
 
 export default MyApp;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,12 @@
+const { spacing, fontFamily } = require('tailwindcss/defaultTheme');
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
+  darkMode: 'class',
   theme: {
     extend: {},
   },
