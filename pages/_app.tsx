8cb8961317955699ffe3dac18164830da1b91b14
Manikanta Inugurthi <manikantajreinstein97@gmail.com>
2022-10-08 15:46:21 +0530

    :sparkles: Update footer to a better layout

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,11 +2,14 @@ import type { AppProps } from "next/app";
 import "../styles/globals.css";
 import { Header } from "../components/Header";
 import { ThemeProvider } from "next-themes";
+import Footer from "../components/Footer";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider attribute="class">
       <Header />
+      <Component {...pageProps} />
+      <Footer />
     </ThemeProvider>
   );
 }
