3e3aee47599d7d1d75c7c67940a43ef645a552f3
Manikanta Inugurthi <manikantajreinstein97@gmail.com>
2023-02-04 19:30:24 +0530

    Remove Snippets and Blog from header

--- data/MenuData.ts
+++ data/MenuData.ts
@@ -9,14 +9,4 @@ export const MenuItems = [
     title: "About",
     transitionDelay: "150ms",
   },
-  {
-    href: "/blog",
-    title: "Blog",
-    transitionDelay: "150ms",
-  },
-  {
-    href: "/snippets",
-    title: "Snippet",
-    transitionDelay: "150ms",
-  },
 ];
