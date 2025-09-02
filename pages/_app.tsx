f2e99a983ba53a5a0232705527d5ee635a3f1605
Manikanta Inugurthi <manikantajreinstein97@gmail.com>
2025-09-02 18:56:40 +0530

    ✨ feat: update layout classes for responsive design in _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,7 +8,7 @@ function MyApp({ Component, pageProps }: AppProps) {
     <div className="dark">
       <div className="min-h-screen bg-gray-900">
         <div className="max-w-6xl mx-auto px-8 py-8">
-          <div className="flex flex-col lg:flex-row lg:gap-16">
+          <div className="flex flex-col md:flex-row md:gap-16">
             <main
               id="skip"
               className="flex-1 max-w-4xl"
@@ -16,16 +16,16 @@ function MyApp({ Component, pageProps }: AppProps) {
               <Component {...pageProps} />
             </main>
             
-            {/* Desktop sidebar - right side */}
-            <aside className="hidden lg:block lg:w-48 lg:flex-shrink-0 mt-8 lg:mt-0">
-              <div className="lg:sticky lg:top-8">
+            {/* Desktop/Tablet sidebar - right side */}
+            <aside className="hidden md:block md:w-48 md:flex-shrink-0 mt-8 md:mt-0">
+              <div className="md:sticky md:top-8">
                 <Sidebar />
               </div>
             </aside>
           </div>
           
           {/* Mobile sidebar - below content */}
-          <aside className="lg:hidden mt-8">
+          <aside className="md:hidden mt-8">
             <Sidebar />
           </aside>
         </div>
